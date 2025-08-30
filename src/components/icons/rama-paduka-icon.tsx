7840cccda6ef0d8d22b17f345b9b70bbfbe3ae47
Chandransh Gupta <gupta.chandransh2004@gmail.com>
2025-08-30 06:33:38 +0000

    now, lets focus on making ram paduka.ico more definitive keep the size s

--- src/components/icons/rama-paduka-icon.tsx
+++ src/components/icons/rama-paduka-icon.tsx
@@ -1,24 +1,35 @@
 
 export function RamaPadukaIcon({ className }: { className?: string }) {
-    // A more detailed representation of Rama's Paduka (sandals)
+    // A more detailed and definitive representation of Rama's Paduka, inspired by ceremonial golden designs.
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 64 64"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2"
+        strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
         className={className}
       >
-        <g transform="scale(1.2) translate(-5, -5)">
-          <path d="M22,52 C22,46 24,40 28,38 C32,36 36,38 36,44 C36,50 32,56 28,58 C24,60 22,58 22,52 Z" />
-          <path d="M29,35 A4,4 0 0,1 33,39 A4,4 0 0,1 29,35" />
-          <path d="M42,52 C42,46 44,40 48,38 C52,36 56,38 56,44 C56,50 52,56 48,58 C44,60 42,58 42,52 Z" />
-          <path d="M49,35 A4,4 0 0,1 53,39 A4,4 0 0,1 49,35" />
-          <path d="M28,30C28,26 30,22 32,22C34,22 36,26 36,30" />
-          <path d="M48,30C48,26 50,22 52,22C54,22 56,26 56,30" />
+        <g transform="translate(4, 2) scale(0.8)">
+            {/* Left Paduka */}
+            <g>
+                {/* Sole Outline */}
+                <path d="M12,58 C4,50 2,38 6,20 S12,2 20,2 S32,10 32,28 C32,38 30,52 22,58" strokeWidth="2.5" />
+                {/* Toe Knob */}
+                <circle cx="24" cy="40" r="5" strokeWidth="2" />
+                <path d="M24,35 A2,2 0 0,1 24,35 L26,33 L28,35 L26,37 L24,39 L22,37 L20,35 L22,33 Z" />
+            </g>
+
+            {/* Right Paduka */}
+            <g transform="translate(32, 0)">
+                {/* Sole Outline */}
+                <path d="M12,58 C4,50 2,38 6,20 S12,2 20,2 S32,10 32,28 C32,38 30,52 22,58" strokeWidth="2.5" />
+                {/* Toe Knob */}
+                <circle cx="24" cy="40" r="5" strokeWidth="2" />
+                <path d="M24,35 A2,2 0 0,1 24,35 L26,33 L28,35 L26,37 L24,39 L22,37 L20,35 L22,33 Z" />
+            </g>
         </g>
       </svg>
     );
