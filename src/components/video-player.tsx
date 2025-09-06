a0882e6eb9763d718342c630286c3b4c77c44bb0
Chandransh Gupta <gupta.chandransh2004@gmail.com>
2025-09-06 22:13:46 +0000

    Remove these recommendation in this player and the top bar including yt

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -55,7 +55,7 @@ export function VideoPlayer({ story, onBeginStory, onBack }: VideoPlayerProps) {
                     key={currentVideo.url}
                     width="100%"
                     height="100%"
-                    src={`${currentVideo.url}?autoplay=1&controls=0&modestbranding=1&rel=0`}
+                    src={`${currentVideo.url}?autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0`}
                     title={currentVideo.title}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
